64b198d62ed9f21ea471d5444a8d9ca0453fd894
Jitesh <jiteshvish64@gmail.com>
2022-05-28 02:35:32 +0530

    Assignment 2

---
+++ Assingnment-2/my-second-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-second-app';
+  username = '';
+  
+  onUpdateProperty(event: Event) {
+    this.username = (event.target as HTMLInputElement).value;
+  }
+}
